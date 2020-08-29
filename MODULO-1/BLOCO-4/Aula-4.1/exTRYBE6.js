3936d3811096df5e327b207bdcdf384212a6155d
jvitorof <j.vitoroflores@gmail.com>
2020-08-28 23:01:28 -0300

    B4/D1/Exercícios realizados exercicio 6

--- MODULO-1/BLOCO-4/Aula-4.1/exTRYBE6.js
+++ MODULO-1/BLOCO-4/Aula-4.1/exTRYBE6.js
@@ -1,6 +1,5 @@
-let piece = king
-let pieces = ["king", "rook", "bishop", "queen", "knight", "pawn"];
-let movements = ["adjacent", "in line h or v", "diagonal", "any line direction", "a three squares l", "one square ahead"]
-
-
-console.log(variavel);
+let piece = "rook"
+let pieces = ['king', 'rook', 'bishop', 'queen', 'knight', 'pawn'];
+let movements = ['adjacent', 'in line h or v', 'diagonal', 'any line direction', 'a three squares l', 'one square ahead'];
+position=pieces.indexOf(piece);
+console.log(`${piece} --> ${movements[position]}`);
