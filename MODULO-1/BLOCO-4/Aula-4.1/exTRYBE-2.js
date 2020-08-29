e86ed55cae14421b2566901b73ea509bfc01470b
jvitorof <j.vitoroflores@gmail.com>
2020-08-28 22:59:53 -0300

    B4/D1/Exercícios realizados

---
+++ MODULO-1/BLOCO-4/Aula-4.1/exTRYBE-2.js
@@ -0,0 +1,8 @@
+let a = 1;
+let b = 2;
+
+if (a < b) {
+    console.log(`Maior é b: ${b}`);
+} else {
+    console.log(`Maior é a: ${a}`)
+}
---
+++ MODULO-1/BLOCO-4/Aula-4.1/script.js
@@ -0,0 +1,11 @@
+let idade = 5;
+console.log(idade);
+let altura = 175;
+console.log(altura);
+
+let nome = 'rafael'; //string literal
+let idadeB = 24; //number literal
+let estaAprovado = true; //boolean
+let sobrenome ; //undefined ou sobrenome = undefined;
+let corSelecionado;// Redefinir um valor.
+// let simbol ;
